f1178ba052bb37a14ba80aa1dd2b6643b01a760a
Ethan Qiu <ethanqiu@gmail.com>
2025-11-10 18:41:31 -0500

    Add pipeline

---
+++ backend/conftest.py
@@ -0,0 +1,20 @@
+"""
+Pytest configuration file for Django tests.
+"""
+import pytest
+from django.conf import settings
+
+
+@pytest.fixture(scope="session")
+def django_db_setup():
+    """
+    Configure the database for testing.
+    """
+    settings.DATABASES["default"] = {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "test_db",
+        "USER": "test_user",
+        "PASSWORD": "test_password",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
