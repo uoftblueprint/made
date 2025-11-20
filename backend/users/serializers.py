507a347750530405daf8b83f3e60b643bf73f5e9
Lily Phan <79292581+pH-li@users.noreply.github.com>
2025-11-20 00:28:16 -0500

    Added intial endpoint setup for volunteer applications with placeholder admin login for getApps

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from .models import User
-
+from .models import VolunteerApplication
 # Create your serializers here.
 
 # Example: User Serializer
@@ -42,3 +42,32 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             role="VOLUNTEER",  # Default role for new signups, should this be configurable
         )
         return user
+
+
+class VolunteerApplicationSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the VolunteerApplication model.
+    Handles creating new volunteer applications upon submission.
+    """
+
+    reviewed_by = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = VolunteerApplication
+        fields = [
+            'id', 
+            'name', 
+            'email', 
+            'motivation_text', 
+            'status', 
+            'created_at',
+            'reviewed_at',
+            'reviewed_by'
+            ]
+        read_only_fields = ['id', 'created_at', 'reviewed_at', 'reviewed_by']
+    
+    def create(self, validated_data):
+        """Create a new volunteer application with PENDING status."""
+        validated_data['status'] = 'PENDING'
+        return super().create(validated_data)
+
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,18 +1,33 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,  # The built-in "Login" view
-    TokenRefreshView,  # The built-in "Refresh Session" view
+    TokenObtainPairView,
+    TokenRefreshView,
 )
+
 from users.views import LogoutView, RegisterView, UserProfileView
+from .views import VolunteerApplicationAPIView
+
+
+# Router setup
+router = DefaultRouter()
+router.register(
+    r"volunteer-applications",
+    VolunteerApplicationAPIView,
+    basename="volunteer-application",
+)
 
 
 urlpatterns = [
-    # The 'api/' part is already handled in core/urls.py
-    # Auth Routes
+    # Auth routes
     path("auth/register/", RegisterView.as_view(), name="auth_register"),
     path("auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("auth/logout/", LogoutView.as_view(), name="auth_logout"),
-    # User Routes
+
+    # User routes
     path("users/me/", UserProfileView.as_view(), name="user_profile"),
+
+    # Router-based routes
+    path("", include(router.urls)),
 ]
