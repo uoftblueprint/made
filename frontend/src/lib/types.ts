eea0ea770226ac37586788fcefe658c6cd70b85d
Edwz208 <edwinzeng99@gmail.com>
2025-11-24 21:07:26 -0500

    added type volunteerApplications

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -6,6 +6,17 @@ export interface User {
   access_expiry_date: string | null;
 }
 
+export interface VolunteerApplication {
+  id: number;
+  name: string;
+  email: string;
+  motivation_text: string;
+  submitted_at: string;
+  status: 'PENDING' | 'APPROVED' | 'REJECTED';
+  reviewed_at: string | null;
+  reviewer: string | null;
+}
+
 // TODO: place holder
 export interface CollectionItem {
   id: number;
