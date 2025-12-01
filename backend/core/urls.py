93e630f652add5d040347bd3be49b5149fb96353
Lily Phan <79292581+pH-li@users.noreply.github.com>
2025-12-01 17:43:50 -0500

    reformat

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     # API Routes
-    path('api/', include('users.urls')),
+    path("api/", include("users.urls")),
     # path('api/inventory/', include('inventory.urls')),
     # path('api/', include('requests.urls')),
 ]
