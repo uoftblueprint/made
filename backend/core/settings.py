1f60f56980ac39089f0632edfe6547985fdfb960
Raymond Chan <raymondch49@gmail.com>
2025-12-17 10:24:55 -0500

    feat(catalogue): removed redundant logic and edited unit tests

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -144,6 +144,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",  # Or JWT
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 SIMPLE_JWT = {
