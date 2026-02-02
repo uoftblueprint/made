16c7132592a8bacd0489b3110d4b73daddf705f6
vivjd <118199397+vivjd@users.noreply.github.com>
2026-02-01 23:38:37 -0500

    reformat

--- backend/movements/apps.py
+++ backend/movements/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MovementsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'movements'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "movements"
