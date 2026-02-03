71325c1d8d26cf901199b09c6905abbec18bbdef
vivjd <118199397+vivjd@users.noreply.github.com>
2026-02-03 00:16:21 -0500

    reformat

--- backend/users/urls_user.py
+++ backend/users/urls_user.py
@@ -6,8 +6,8 @@ router = DefaultRouter()
 router.register(r"volunteer-applications", VolunteerApplicationAPIView, basename="volunteer-application")
 
 urlpatterns = [
-    path("", include(router.urls)), # This handles /api/users/volunteer-applications/
-    path("me/", UserProfileView.as_view(), name="user_profile"), # /api/users/me/
-    path("list/", UserListView.as_view(), name="user_list"), # /api/users/list/
-    path("<int:pk>/", UserUpdateView.as_view(), name="user_update"), # /api/users/1/
+    path("", include(router.urls)),  # This handles /api/users/volunteer-applications/
+    path("me/", UserProfileView.as_view(), name="user_profile"),  # /api/users/me/
+    path("list/", UserListView.as_view(), name="user_list"),  # /api/users/list/
+    path("<int:pk>/", UserUpdateView.as_view(), name="user_update"),  # /api/users/1/
 ]
