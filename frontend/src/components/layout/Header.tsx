6424fdad26253f466494662c237b668f78612389
vivjd <118199397+vivjd@users.noreply.github.com>
2026-02-03 23:59:09 -0500

    remove protected route from header to avoid redirect

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import ProtectedRoute from '../ProtectedRoute';
 
 const Header = () => {
   return (
@@ -7,15 +6,9 @@ const Header = () => {
       <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
       <Link to="/catalogue" style={{ marginRight: '1rem' }}>Catalogue</Link>
       <Link to="/volunteer_application" style={{ marginRight: '1rem' }}>Volunteer Application</Link>
-      <ProtectedRoute>
-      <ProtectedRoute requiredRole="ADMIN">
         <Link to="/admin" style={{ marginRight: '1rem' }}>Admin Dashboard</Link>
-      </ProtectedRoute>
-        <Link to="/login" style={{ marginRight: '1rem' }}>Login</Link>
-      </ProtectedRoute>
-      <ProtectedRoute>
+      <Link to="/login" style={{ marginRight: '1rem' }}>Login</Link>
         <Link to="/logout" style={{ marginRight: '1rem' }}>Logout</Link>
-      </ProtectedRoute>
     </nav>
   );
 };
